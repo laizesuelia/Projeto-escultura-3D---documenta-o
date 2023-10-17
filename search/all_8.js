ea3793cf7e666c7c58690b0554957129fa8241da
laizesuelia <68816133+laizesuelia@users.noreply.github.com>
2023-10-17 20:21:17 -0300

    first commit

---
+++ search/all_8.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['sculptor_0',['sculptor',['../class_sculptor.html',1,'Sculptor'],['../class_sculptor.html#a58c1d483677d81657acae69e0972e55b',1,'Sculptor::Sculptor()']]],
+  ['sculptor_2e_2ecpp_1',['Sculptor..cpp',['../_sculptor_8_8cpp.html',1,'']]],
+  ['sculptor_2eh_2',['Sculptor.h',['../_sculptor_8h.html',1,'']]],
+  ['setcolor_3',['setColor',['../class_sculptor.html#a4351c930a07887e96d4fdbf251a7867e',1,'Sculptor']]],
+  ['show_4',['show',['../struct_voxel.html#a7daa6a3073416618d1365e0fc38f152f',1,'Voxel']]]
+];
---
+++ search/functions_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sculptor_0',['Sculptor',['../class_sculptor.html#a58c1d483677d81657acae69e0972e55b',1,'Sculptor']]],
+  ['setcolor_1',['setColor',['../class_sculptor.html#a4351c930a07887e96d4fdbf251a7867e',1,'Sculptor']]]
+];
